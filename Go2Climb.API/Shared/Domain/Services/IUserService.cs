e0c4aa5af5227cdbc595a93ccd879190a231f846
jrenzzo <u202118568@upc.edu.pe>
2023-04-20 22:50:32 -0500

    chore: add shared bounded context

---
+++ Go2Climb.API/Shared/Domain/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Go2Climb.API.Domain.Models;
+using Go2Climb.API.Domain.Services.Communication;
+using Go2Climb.API.Security.Domain.Services.Communication;
+
+namespace Go2Climb.API.Customers.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+    }
+}
---
+++ Go2Climb.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+using Go2Climb.API.Agencies.Domain.Models;
+using Go2Climb.API.Domain.Models;
+using Go2Climb.API.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Go2Climb.API.Shared.Persistence.Contexts
+{
+
+}
